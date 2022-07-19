11d68970f513052134dd7d3a069043c0f725dfc9
isahyu <106920782+isahyu@users.noreply.github.com>
2022-07-18 23:00:15 -0300

    Projeto Sekihouse

---
+++ SekiHouseproject-Atos/src/main/java/View/Main.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+import Model.Agendamento;
+import Model.Cliente;
+import Model.Pessoa;
+import Model.Servico;
+import Model.Usuario;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class Main {
+    
+    
+   public static void main(String[] args){
+   
+       String nome = "Lucas";
+       System.out.println(nome);
+       
+       Servico servico = new Servico(1, "tattoo", 100);
+       System.out.println(servico.getDescricao());
+       System.out.println(servico.getValor());
+       
+       Cliente cliente = new Cliente("rua teste", "86600278", 1, "Sekitani Alice");
+       System.out.println(cliente.getNome());
+       
+       Usuario usuario = new Usuario(1, "Lucas", "211806");
+       System.out.println(usuario.getNome());
+       
+       
+       Agendamento agendamento = new Agendamento(1, cliente, servico, 100, "06/10/2050 09:15");
+       System.out.println(agendamento.getCliente().getNome());
+       
+       
+     
+   }
+}
